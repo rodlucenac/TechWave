ac5eecd1b4719c80c27d280c874ca883df7d3f03
rodlucenac <digolucena14@gmail.com>
2025-03-21 00:35:15 -0300

    Update Backend

---
+++ Back/techwave/src/main/java/br/com/backend/techwave/TechwaveApplication.java
@@ -0,0 +1,13 @@
+package br.com.backend.techwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechwaveApplication.class, args);
+	}
+
+}
